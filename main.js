d41b325bbfc2198cb8751669b2900e289e2ce7a8
Alexander <ael5627@psu.edu>
2024-08-25 18:43:46 -0400

    branch

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 (function() {
     'use strict'
+    const menu = document.getElementById("menu");
+    const closeButton = document.querySelector("#close");
+    const openButton = document.querySelector("h1");
+    closeButton.addEventListener("click", () => menu.style.display = "none"); 
+    openButton.addEventListener("click", () => menu.style.display = "block"); 
 
     console.log('main.js loaded');
+    console.log(menu);
 
 }())
